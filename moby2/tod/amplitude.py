62f161264882d2acde99c6e56a9e8de122c79363
Erika Hornecker <36018040+erika-hornecker@users.noreply.github.com>
2025-02-25 19:34:10 -0500

    added peak excision

--- moby2/tod/amplitude.py
+++ moby2/tod/amplitude.py
@@ -21,6 +21,35 @@ from scipy.optimize import curve_fit
 from astropy.coordinates import SkyCoord
 
 
+def excise_peaks(snip_data, buff):
+    new_snip = snip_data.copy()
+    for i in range(len(snip_data)):
+        det_vals = snip_data[i]
+        try:
+            #buffer around peak value
+
+            kernel_size = 8
+            kernel = np.ones(kernel_size) / kernel_size
+
+            kernel_smooth = 50
+            kernel_2 = np.ones(kernel_smooth) / kernel_smooth
+
+            mean_vals1 = np.convolve(det_vals, kernel, mode='same')
+            smooth_vals = np.convolve(mean_vals1, kernel_2, mode='same')
+            vals_use = mean_vals1-smooth_vals
+            std_use = np.std(vals_use[50:-50])
+
+            peak_idx = sp.signal.find_peaks(vals_use, prominence = std_use*4)[0][0]
+
+            x_interp = [i for i in range(peak_idx-buff, peak_idx+buff)]
+            y_interp = np.interp(x_interp, [peak_idx-(buff+1), peak_idx+buff], [mean_vals1[peak_idx-(buff+1)], mean_vals1[peak_idx+buff]])
+ 
+            new_snip[i][x_interp] = y_interp
+        except Exception as e:
+            print('failed to excise peak')
+            print(e)
+            continue
+    return new_snip
 
 def amp_fit(det, amp, pos_ra, pos_dec, off):
 
